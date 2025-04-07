adb0cd81d4480aff758cef96a014a776e10ccbf4
Egor Kurtashkin <egorkurtashkin@gmail.com>
2025-04-07 18:03:54 +0300

    sand physics

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{io::{self, stdout, Write}, thread::sleep, time::Duration};
 use crossterm::{
     cursor::{Hide, MoveTo}, event::poll, execute, queue, style::Print, terminal::{size, Clear, ClearType}
 };
-use rand;
+use rand::{self, random_bool};
 
 struct GameWorld {
     width: usize,
@@ -27,11 +27,11 @@ impl GameWorld {
         if x == 0 {
             return ' ';
         }
-        '#'
+        '*'
     }
 
     fn render(&mut self) -> io::Result<()> {
-        for x in 0..self.current.len() {
+        for x in 0..self.width*self.height {
             let x_pos = (x % self.width) as u16;
             let y_pos = (x / self.width) as u16;
             queue!(stdout(), MoveTo(x_pos, y_pos), Print(Self::convert_to_char(self.current[x])))?;
@@ -41,27 +41,68 @@ impl GameWorld {
         Ok(())
     }
 
-    fn get_current_cell_below(&mut self, x: usize) -> Option<&mut u8> {
+    fn get_previous_cell_below(&mut self, x: usize) -> Option<u8> {
         if (x / self.width) == self.height - 1 {
             return None;
         }
-        Some(&mut self.current[x + self.width])
+        Some(self.current[x + self.width])
     }
 
-    fn get_previous_cell_below(&mut self, x: usize) -> Option<&mut u8> {
+    fn get_previous_diagonal_cell_below(&mut self, x: usize) -> (Option<u8>, Option<u8>) {
         if (x / self.width) == self.height - 1 {
-            return None;
+            return (None, None);
         }
-        Some(&mut self.current[x + self.width])
+        
+        if (x % self.width) == self.width - 1 {
+            return (Some(self.current[x + self.width - 1]), None);
+        }
+
+        if (x % self.width) == 0 {
+            return (None, Some(self.current[x + self.width + 1]));
+        }
+
+        (Some(self.previous[x + self.width - 1]), Some(self.previous[x + self.width + 1]))
+    }
+
+    fn fall_down(&mut self, x: usize) {
+        self.current[x] = 0;
+        self.current[x + self.width] = 1;
+    }
+
+    fn fall_left(&mut self, x: usize) {
+        self.current[x] = 0;
+        self.current[x + self.width - 1] = 1;
+    }
+
+    fn fall_right(&mut self, x: usize) {
+        self.current[x] = 0;
+        self.current[x + self.width + 1] = 1;
     }
 
     fn update(&mut self) {
-        for x in 0..self.current.len() {
+        for x in (0..self.width*self.height).rev() {
             if self.previous[x] == 1 {
-                if let Some(prev_cell) = self.get_previous_cell_below(x) {
-                    if *prev_cell == 0 {
-                        self.current[x] = 0;
-                        *self.get_current_cell_below(x).unwrap() = 1;
+                if self.get_previous_cell_below(x).is_some_and(|x| x == 0) {
+                    self.fall_down(x);
+                }
+                else {
+                    let (left, right) = self.get_previous_diagonal_cell_below(x);
+
+                    if left.is_some_and(|x| x == 0) && right.is_some_and(|x| x == 0) {
+                        if random_bool(0.5) {
+                            self.fall_left(x);
+                        }
+                        else {
+                            self.fall_right(x);
+                        }
+                    }
+
+                    if left.is_some_and(|x| x == 0) && (right.is_none() || right.is_some_and(|x| x == 1)) {
+                        self.fall_left(x);
+                    }
+
+                    if right.is_some_and(|x| x == 0) && (left.is_none() || left.is_some_and(|x| x == 1)) {
+                        self.fall_right(x);
                     }
                 }
             }
@@ -75,10 +116,33 @@ fn main() -> io::Result<()> {
     let mut game_world = GameWorld::new()?;
     game_world.render()?;
     loop {
-        game_world.current[rand::random_range(0..game_world.width)] = 1;
+        // game_world.current[rand::random_range(0..game_world.width)] = 1;
+        game_world.current[game_world.width / 2] = 1;
+        game_world.current[game_world.width / 3] = 1;
+        game_world.current[game_world.width / 3 * 2] = 1;
         game_world.render()?;
         game_world.update();
         sleep(Duration::from_millis(100));
     }
+
+    // game_world.current[game_world.width * game_world.height - 10] = 1;
+    // game_world.current[game_world.width * game_world.height - 11] = 1;
+    // game_world.current[game_world.width * game_world.height - 12] = 1;
+    // game_world.current[game_world.width * (game_world.height - 1) - 10] = 1;
+    // game_world.current[game_world.width * (game_world.height - 1) - 12] = 1;
+    // game_world.current[game_world.width * (game_world.height - 2) - 10] = 1;
+    // game_world.current[game_world.width * (game_world.height - 2) - 12] = 1;
+    // game_world.current[game_world.width * (game_world.height - 3) - 10] = 1;
+    // game_world.current[game_world.width * (game_world.height - 3) - 12] = 1;
+    // game_world.render()?;
+    // sleep(Duration::from_millis(100));
+    // game_world.update();
+    // game_world.render()?;
+    // game_world.update();
+    // game_world.render()?;
+    // game_world.update();
+    // game_world.render()?;
+    // game_world.update();
+    // game_world.render()?;
     Ok(())
 }
